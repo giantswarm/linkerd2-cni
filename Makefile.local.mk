65fd90e0f58b392df4d674c52ea932c08aec9050
Matias Charriere <matias@giantswarm.io>
2022-10-19 11:15:31 +0200

    update dependencies

--- Makefile.local.mk
+++ Makefile.local.mk
@@ -5,15 +5,23 @@ VENDOR_CHART_CNI=vendor/linkerd/charts/linkerd2-cni
 VENDOR_CHART_PARTIALS=vendor/linkerd/charts/partials
 
 
-.PHONY: update-chart helm-docs
+.PHONY: update-chart helm-docs apply-vendor update-deps
 update-chart: ## Run vendir sync
 	@echo "====> $@"
 	vendir sync
+	$(MAKE) apply-vendor
+	$(MAKE) update-deps
+
+update-deps:
+	cd $(MAIN_CHART) && helm dependency update
+
+helm-docs:
+	helm-docs -c $(MAIN_CHART) -g $(MAIN_CHART)
+
+apply-vendor:
 	mkdir -p $(MAIN_CHART)/charts
 	rm -rf $(MAIN_CHART)/templates
 	rm -rf $(MAIN_CHART)/charts/partials
 	cp -r $(VENDOR_CHART_CNI)/templates $(MAIN_CHART)
 	cp -r $(VENDOR_CHART_PARTIALS) $(MAIN_CHART)/charts/
 
-helm-docs:
-	helm-docs -u -c $(MAIN_CHART) -g $(MAIN_CHART)
